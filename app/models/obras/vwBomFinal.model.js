e762ed997e51c7ef98c5ca6c64b903d43aad0771
Hector Miranda <hectormiranda.gonzalez@gmail.com>
2024-04-01 11:16:26 -0300

    v1.6.27 agrega copnsulta a bom_zero y bom_final

---
+++ app/models/obras/vwBomFinal.model.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, Sequelize) => {
+    const VwBomFinal = sequelize.define("view_bom_final", {
+        id: {
+            type: Sequelize.BIGINT,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        id_obra: {
+            type: Sequelize.BIGINT,
+            allowNull: false,
+        },
+        cod_reserva: {
+            type: Sequelize.BIGINT,
+            allowNull: false
+        },
+        codigo_sap_material: {
+            type: Sequelize.BIGINT,
+            allowNull: false
+        },
+        cantidad_requerida: {
+            type: Sequelize.FLOAT,
+            allowNull: false
+        },
+        fecha_ingreso: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        rut_usuario: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        persona: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    },
+    {
+        schema: "obras",
+    });
+    return VwBomFinal;
+  };
