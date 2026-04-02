ac55b370f42e73aad8bf3d6d4d55b07f7b16936d
pelomING <141353142+pelomING@users.noreply.github.com>
2026-04-02 20:16:03 -0300

    Add new allowed origin to server configuration

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(
     "https://localhost:5557",
     "http://181.42.20.52", 
     "http://186.11.3.23", 
+    "http://151.101.2.15",
     "https://siscop.up.railway.app", 
     "https://pelom-ing.up.railway.app",
     "https://pelom-ing-dev.up.railway.app",
